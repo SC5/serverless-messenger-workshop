a4c7f31d81554a4a3fc1275810aee3582ed5e6df
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-11-04 12:38:47 +0200

    Initial commit

---
+++ facebook-bot/handler.js
@@ -0,0 +1,38 @@
+'use strict';
+
+require('../lib/envVars').config();
+const messenger = require('./fb-messenger.js');
+const messageQueue = require('../lib/messageQueue.js');
+
+/**
+ * Adds extra environmental variables
+ * @param event
+ */
+function setEnvVars(event) {
+  process.env.SERVERLESS_STAGE = event.stage;
+}
+
+module.exports.handler = (event, context, cb) => {
+  setEnvVars(event);
+  if (event.method === 'GET') {
+    return messenger.verify(event, cb);
+  } else if (event.method === 'POST') {
+    return messenger.receive(event, cb);
+  } else if (event.Records && event.Records[0] && event.Records[0].Sns) {
+    return messageQueue.getMessage(event)
+      .then((queueEvent) => {
+        const message = queueEvent.message;
+        setEnvVars(queueEvent);
+        if (process.env.SILENT) {
+          return queueEvent;
+        }
+        return messenger.sendMessage(queueEvent.recipient.id, message);
+      })
+      .then(result => cb(null, result))
+      .catch((error) => {
+        console.log('ERROR, handler', error);
+        cb(error);
+      });
+  }
+  return cb('Unknown event');
+};
